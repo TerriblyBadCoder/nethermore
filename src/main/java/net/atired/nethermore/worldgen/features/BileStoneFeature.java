12cf7ccb1a2d729f4aade8f8a3e7a4dba11b87a7
AtiredGuy <matveiqwerty12@gmail.com>
2026-09-21 22:54:51 +0300

    wip bilegen

--- src/main/java/net/atired/nethermore/worldgen/features/BileStoneFeature.java
+++ src/main/java/net/atired/nethermore/worldgen/features/BileStoneFeature.java
@@ -2,11 +2,15 @@ package net.atired.nethermore.worldgen.features;
 
 import com.mojang.serialization.Codec;
 import net.atired.nethermore.init.NMBlockInit;
+import net.atired.nethermore.misc.NMsimplexNoise;
 import net.minecraft.core.BlockPos;
+import net.minecraft.tags.BlockTags;
 import net.minecraft.world.level.WorldGenLevel;
 import net.minecraft.world.level.levelgen.feature.Feature;
 import net.minecraft.world.level.levelgen.feature.FeaturePlaceContext;
 import net.minecraft.world.level.levelgen.feature.configurations.NoneFeatureConfiguration;
+import net.minecraft.world.level.material.Fluids;
+import net.minecraft.world.phys.Vec3;
 
 public class BileStoneFeature extends Feature<NoneFeatureConfiguration> {
     public BileStoneFeature(Codec<NoneFeatureConfiguration> codec) {
@@ -23,7 +27,28 @@ public class BileStoneFeature extends Feature<NoneFeatureConfiguration> {
         for (int x = -12; x <= 12; x++) {
             for (int y = -12; y <= 12; y++) {
                 for (int z = -12; z <= 12; z++) {
-
+                    BlockPos pos1=pos.offset(x,y,z);
+                    float noisy = NMsimplexNoise.sampleNoise3D(pos1.getX(),pos1.getY()/20.0f+300,pos1.getZ(),55.0f);
+                    noisy=Math.abs(noisy);
+                    noisy=noisy%0.2f;
+                    noisy*=5.0f;
+                    double len = 10.0f/new Vec3(12,12,12).length();
+                    if(noisy*len>0.2){
+                        if((!level.isEmptyBlock(pos1)&&(level.getBlockState(pos1).isSolid()||level.getBlockState(pos1).is(BlockTags.SOUL_FIRE_BASE_BLOCKS)))||
+                                (!level.isEmptyBlock(pos1.below())&&noisy*len>0.3)){
+                            if(!level.getBlockState(pos1).is(NMBlockInit.TOPPED_BILESTONE)&&!level.getFluidState(pos1).is(Fluids.LAVA)&&!level.getBlockState(pos1.below()).is(NMBlockInit.TOPPED_BILESTONE)){
+                                if(level.isEmptyBlock(pos1.above())){
+                                    noisy = NMsimplexNoise.sampleNoise3D(pos1.getX(),pos1.getY()/20.0f+300+1/20.0f,pos1.getZ(),55.0f);
+                                    noisy=noisy%0.2f;
+                                    noisy*=5.0f;
+                                    if(level.isEmptyBlock(pos1))noisy*=0.1f;
+                                    level.setBlock(pos1,(noisy*len>0.3?NMBlockInit.BILESTONE.get():NMBlockInit.TOPPED_BILESTONE.get()).defaultBlockState(),2);
+                                }else{
+                                    level.setBlock(pos1,(NMBlockInit.BILESTONE.get()).defaultBlockState(),2);
+                                }
+                            }
+                        }
+                    }
                 }
             }
         }
@@ -35,12 +60,12 @@ public class BileStoneFeature extends Feature<NoneFeatureConfiguration> {
                     for (int y = -1; y <= i+2; y++) {
                         BlockPos pos1 = pos.offset(x+offX,y,z+offZ);
                         if(level.isEmptyBlock(pos1)&&level.getBlockState(pos1.below()).isSolid()){
-                            if(level.getBlockState(pos1.below()).is(NMBlockInit.TOPPED_BILESTONE)){
-                                level.setBlock(pos1.below(),(NMBlockInit.BILESTONE.get()).defaultBlockState(),2);
 
-                            }
                             if(y!=i+2){
                                 level.setBlock(pos1,((level.isEmptyBlock(pos1.above())&&y==i+1)? NMBlockInit.TOPPED_BILESTONE.get():NMBlockInit.BILESTONE.get()).defaultBlockState(),2);
+                                if(level.getBlockState(pos1.below()).is(NMBlockInit.TOPPED_BILESTONE)){
+                                    level.setBlock(pos1.below(),(NMBlockInit.BILESTONE.get()).defaultBlockState(),2);
+                                }
                             }else if(Math.random()>0.9){
                                 level.setBlock(pos1,(NMBlockInit.BILE_GROWTH.get()).defaultBlockState(),2);
                             }
